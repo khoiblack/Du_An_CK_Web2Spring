00672fce6f3cc3b7b98bbd2d7847d67989aa4c39
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 21:06:35 +0700

    tạo webconfig

---
+++ LabEquipmentManagement/src/main/java/ntu/khoi/config/WebConfig.java
@@ -0,0 +1,21 @@
+package ntu.khoi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login", "/logout", "/css/**", "/js/**", "/vendor/**");
+    }
+}
