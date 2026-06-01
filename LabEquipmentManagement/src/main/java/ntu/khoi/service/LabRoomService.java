f52d913c0725c690fb3eb076eb53cc1a8cae8e16
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 21:12:31 +0700

    tạo labroom

---
+++ LabEquipmentManagement/src/main/java/ntu/khoi/service/LabRoomService.java
@@ -0,0 +1,22 @@
+package ntu.khoi.service;
+
+import ntu.khoi.entity.LabRoom;
+import ntu.khoi.repository.LabRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class LabRoomService {
+
+    @Autowired
+    private LabRoomRepository labRoomRepository;
+
+    public List<LabRoom> getAll() {
+        return labRoomRepository.findAll();
+    }
+
+    public LabRoom getById(Integer id) {
+        return labRoomRepository.findById(id).orElse(null);
+    }
+}
