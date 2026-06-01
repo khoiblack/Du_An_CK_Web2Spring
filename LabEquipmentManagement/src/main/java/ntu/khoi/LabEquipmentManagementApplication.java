611eb1511a2612a7cea5cdcf4b6de557f0c167d9
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 14:10:52 +0700

    tạo dự án quản lí mượn thiết bị

---
+++ LabEquipmentManagement/src/main/java/ntu/khoi/LabEquipmentManagementApplication.java
@@ -0,0 +1,13 @@
+package ntu.khoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabEquipmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabEquipmentManagementApplication.class, args);
+	}
+
+}
---
+++ LabEquipmentManagement/src/test/java/ntu/khoi/LabEquipmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.khoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabEquipmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
