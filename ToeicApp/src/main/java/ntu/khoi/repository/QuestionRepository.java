b3d7bed062ce11bb6d8be6a88b1c83e73fa22cb0
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 11:23:30 +0700

    update code

--- ToeicApp/src/main/java/ntu/khoi/repository/QuestionRepository.java
+++ ToeicApp/src/main/java/ntu/khoi/repository/QuestionRepository.java
@@ -1,5 +1,13 @@
 package ntu.khoi.repository;
 
-public class QuestionRepository {
+import ntu.khoi.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    
+    List<Question> findByExamId(Integer examId);
 }
