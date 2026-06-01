c671999dae2d3d86111cb9798e96e916b78c18b9
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 11:23:16 +0700

    update code

--- ToeicApp/src/main/java/ntu/khoi/repository/TestResultRepository.java
+++ ToeicApp/src/main/java/ntu/khoi/repository/TestResultRepository.java
@@ -1,5 +1,13 @@
 package ntu.khoi.repository;
 
-public class TestResultRepository {
+import ntu.khoi.models.TestResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface TestResultRepository extends JpaRepository<TestResult, Integer> {
+    
+    List<TestResult> findByUserIdOrderByTestDateDesc(Integer userId);
 }
