26298f62ce32ae3c56a05b37a4a22ef5077bf692
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 11:23:03 +0700

    update code

--- ToeicApp/src/main/java/ntu/khoi/repository/UserRepository.java
+++ ToeicApp/src/main/java/ntu/khoi/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package ntu.khoi.repository;
 
-public class UserRepository {
+import ntu.khoi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+    Optional<User> findByUsername(String username);
 }
