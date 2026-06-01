202474a427b13586596898281a9ad6ff664b68cb
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2026-06-01 11:22:50 +0700

    update code

--- ToeicApp/src/main/java/ntu/khoi/repository/ExamRepository.java
+++ ToeicApp/src/main/java/ntu/khoi/repository/ExamRepository.java
@@ -1,5 +1,10 @@
 package ntu.khoi.repository;
 
-public class ExamRepository {
+import ntu.khoi.models.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface ExamRepository extends JpaRepository<Exam, Integer> {
+    
 }
